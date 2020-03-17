35ce77da94b6bfb46c6f2f300aef4709dfd4cbec
AaEzha <rezanurfachmi@gmail.com>
2020-03-17 16:22:22 +0700

    Featured Product

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -35,5 +35,44 @@
     </div>
   </div>
 </section>
+
+<section class="section">
+  <div class="container">
+
+    <header class="section-header">
+      <h2>Featured Products</h2>
+      <p class="lead">Produce and supply various Handicraft items all over the world which were very attractive</p>
+    </header>
+
+    <div class="row gap-y">
+
+      <?php for($i=1; $i<=3; $i++): ?>
+      <div class="col-md-6 col-xl-4">
+        <div class="product-3 mb-3">
+          <a class="product-media" href="#">
+            <img src="{{ asset('images/14.jpg') }}" alt="product">
+          </a>
+          <div class="product-detail text-left">
+            <h6><a href="#">Apple EarPods</a></h6>
+            <div class="product-price"><del>$200</del> $160</div>
+            <div>
+              <i class="fa fa-star" aria-hidden="true"></i> 
+              <i class="fa fa-star" aria-hidden="true"></i> 
+              <i class="fa fa-star" aria-hidden="true"></i> 
+              <i class="fa fa-star" aria-hidden="true"></i> 
+              <i class="fa fa-star-o" aria-hidden="true"></i>
+              <span class="pull-right">Music</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <?php endfor; ?>
+
+    </div>
+
+  </div>
+</section>
+
+
       
 @endsection
